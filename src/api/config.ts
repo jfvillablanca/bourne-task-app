2da9645af41e69ab3d9fc9f93009355c7e11f048
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-06-13 15:20:04 +0800

    Deps: setup axios client

---
+++ src/api/config.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+});
+
+apiClient.interceptors.request.use(
+    (config) => {
+        return {
+            ...config,
+            // headers: {
+            //   ...
+            // },
+        };
+    },
+    (error) => Promise.reject(error),
+);
+
+apiClient.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        // ...
+        return Promise.reject(error.response.data);
+    },
+);
+
+const { get, post, patch, delete: destroy } = apiClient;
+export { destroy, get, patch, post };
