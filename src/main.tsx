437630083113560c23e3ec11806686c36752422e
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-06-13 15:39:48 +0800

    Deps: setup mock service worker

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,27 @@ import ReactDOM from 'react-dom/client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+// @ts-expect-error Used by mockServiceWorker and has no declaration file
+import { worker } from './mocks/browser';
 import App from './App.tsx';
 
 import './styles/globals.css';
 
-const queryClient = new QueryClient();
+async function main() {
+    if (import.meta.env.DEV) {
+        await worker.start();
+    }
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <App />
-            <ReactQueryDevtools />
-        </QueryClientProvider>
-    </React.StrictMode>,
-);
+    const queryClient = new QueryClient();
+
+    ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+        <React.StrictMode>
+            <QueryClientProvider client={queryClient}>
+                <App />
+                <ReactQueryDevtools />
+            </QueryClientProvider>
+        </React.StrictMode>,
+    );
+}
+main();
