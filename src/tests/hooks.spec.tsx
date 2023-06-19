200312c62dec3f4ff67053a76bd58f67f202e157
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-06-20 07:23:48 +0800

    Test(hooks): add findAll projects test

---
+++ src/tests/hooks.spec.tsx
@@ -0,0 +1,27 @@
+import { setupServer } from 'msw/node';
+
+import { renderHook, waitFor } from '@testing-library/react';
+
+import { Project } from '../api';
+
+import { createWrapper, handlers } from './utils';
+
+const server = setupServer(...handlers);
+
+beforeAll(() => server.listen());
+
+afterEach(() => server.resetHandlers());
+
+afterAll(() => server.close());
+
+describe('Project', () => {
+    it('should findAll projects of a user', async () => {
+        const { result } = renderHook(() => Project.useFindAll(), {
+            wrapper: createWrapper(),
+        });
+
+        await waitFor(() => expect(result.current.data).toBeDefined());
+
+        expect(result.current.data).toHaveLength(5);
+    });
+});
