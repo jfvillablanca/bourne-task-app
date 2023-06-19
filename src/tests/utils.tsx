8551799e0ac66fb28b3402e8edd00e8f3d1a0200
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-06-20 07:23:12 +0800

    Test(chore): setup test utils with msw integration

---
+++ src/tests/utils.tsx
@@ -0,0 +1,57 @@
+import { HttpStatusCode } from 'axios';
+import { rest } from 'msw';
+import { ReactNode } from 'react';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { render } from '@testing-library/react';
+
+import { mockProjects } from '../mocks/fixtures';
+
+export const handlers = [
+    rest.get('/api/projects', (_req, res, ctx) => {
+        return res(ctx.json(mockProjects()), ctx.status(HttpStatusCode.Ok));
+    }),
+];
+
+const createTestQueryClient = () =>
+    new QueryClient({
+        logger: {
+            log: console.log,
+            warn: console.warn,
+            error: process.env.NODE_ENV === 'test' ? () => {} : console.error,
+        },
+        defaultOptions: {
+            queries: {
+                retry: false,
+            },
+        },
+    });
+
+// Used for hooks testing
+export function createWrapper() {
+    const testQueryClient = createTestQueryClient();
+    return ({ children }: { children: ReactNode }) => (
+        <QueryClientProvider client={testQueryClient}>
+            {children}
+        </QueryClientProvider>
+    );
+}
+
+// Used for component testing
+export function renderWithClient(ui: React.ReactElement) {
+    const testQueryClient = createTestQueryClient();
+    const { rerender, ...result } = render(
+        <QueryClientProvider client={testQueryClient}>
+            {ui}
+        </QueryClientProvider>,
+    );
+    return {
+        ...result,
+        rerender: (rerenderUi: React.ReactElement) =>
+            rerender(
+                <QueryClientProvider client={testQueryClient}>
+                    {rerenderUi}
+                </QueryClientProvider>,
+            ),
+    };
+}
