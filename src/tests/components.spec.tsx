9dad4c0898a8ff82c5119cd265b62ec9c8a9aa26
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-06-21 20:12:45 +0800

    Test(comp): add CardView specs

--- src/tests/components.spec.tsx
+++ src/tests/components.spec.tsx
@@ -3,6 +3,8 @@ import { setupServer } from 'msw/node';
 import { screen, waitFor } from '@testing-library/react';
 
 import App from '../App';
+import { TaskDocument } from '../common';
+import { CardView } from '../components';
 import { mockProjects } from '../mocks/fixtures';
 import { handlers } from '../mocks/handlers';
 
@@ -36,6 +38,46 @@ describe('App', () => {
         await waitFor(() => {
             mockProjectTasks.forEach((task) => {
                 expect(screen.getByText(task.title)).toBeInTheDocument();
+            });
+        });
+    });
+});
+
+describe('CardView', () => {
+    it('should render task card groups', async () => {
+        const mockProjectId = mockProjects()[0]._id;
+        const mockProjectTaskStates = mockProjects()[0].taskStates;
+        const mockTasks = mockProjects()[0].tasks;
+        const groupedTasks = mockTasks.reduce((acc, task) => {
+            const { taskState } = task;
+            if (!acc[taskState]) {
+                acc[taskState] = [];
+            }
+            acc[taskState].push(task);
+            return acc;
+        }, {} as { [key: string]: TaskDocument[] });
+
+        renderWithClient(<CardView projectId={mockProjectId} />);
+
+        await waitFor(() => {
+            mockProjectTaskStates.forEach((taskState) => {
+                const tasksInState = groupedTasks[taskState];
+                tasksInState.forEach((task) => {
+                    expect(screen.getByText(task.title)).toBeInTheDocument();
+                });
+            });
+        });
+    });
+
+    it('should correctly group tasks to the correct task card group', async () => {
+        const mockProjectId = mockProjects()[0]._id;
+        const mockProjectTaskStates = mockProjects()[0].taskStates;
+
+        renderWithClient(<CardView projectId={mockProjectId} />);
+
+        await waitFor(() => {
+            mockProjectTaskStates.forEach((taskState) => {
+                expect(screen.getByText(taskState)).toBeInTheDocument();
             });
         });
     });
