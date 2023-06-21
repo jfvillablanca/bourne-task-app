322754eaee04310930864e1a63e6816568286a9b
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-06-21 12:41:03 +0800

    Chore(config): add vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,7 @@ export default defineConfig({
     test: {
         globals: true,
         environment: 'jsdom',
+        include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+        setupFiles: './setupTests.ts',
     },
 });
