2f65de6e1854a9090668ebfacc3b979c9b977362
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-06-22 20:39:25 +0800

    Feat: use localStorage as mock database

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,15 +2,31 @@ import { HttpStatusCode } from 'axios';
 import { rest } from 'msw';
 
 import { mockProjects } from './fixtures';
+import { ProjectDocument } from '../common';
 
+
+
+const PROJECTS_STORAGE_KEY = 'projects';
+
+const getProjectsFromStorage = (): ProjectDocument[] => {
+    const storedData = localStorage.getItem(PROJECTS_STORAGE_KEY);
+    if (storedData) {
+        return JSON.parse(storedData);
+    } else {
+        const initialData = mockProjects();
+        localStorage.setItem(PROJECTS_STORAGE_KEY, JSON.stringify(initialData));
+        return initialData;
+    }
+};
 export const handlers = [
     rest.get('/api/projects', (_req, res, ctx) => {
-        return res(ctx.json(mockProjects()), ctx.status(HttpStatusCode.Ok));
+        const projects = getProjectsFromStorage();
+        return res(ctx.json(projects), ctx.status(HttpStatusCode.Ok));
     }),
 
     rest.get('/api/projects/:projectId', (req, res, ctx) => {
         const { projectId } = req.params;
-        const project = mockProjects().find(
+        const project = getProjectsFromStorage().find(
             (project) => project._id === projectId,
         );
         return res(ctx.json(project), ctx.status(HttpStatusCode.Ok));
@@ -18,7 +34,7 @@ export const handlers = [
 
     rest.get('/api/projects/:projectId/tasks', (req, res, ctx) => {
         const { projectId } = req.params;
-        const tasks = mockProjects().find(
+        const tasks = getProjectsFromStorage().find(
             (project) => project._id === projectId,
         )?.tasks;
 
