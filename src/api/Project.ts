b7605c14395620f582cfcb495ff714ce84f1ff89
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-06-23 11:39:11 +0800

    Feat: add Project.useUpdate hook

--- src/api/Project.ts
+++ src/api/Project.ts
@@ -1,8 +1,8 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
-import { ProjectDocument } from '../common';
+import { ProjectDocument, UpdateProjectDto } from '../common';
 
-import { get } from '.';
+import { get, patch } from '.';
 
 export const Project = {
     queryKeys: {
@@ -21,7 +21,28 @@ export const Project = {
             queryKey: Project.queryKeys.byId(projectId),
             queryFn: () => getProjectById(projectId),
         }),
+
+    useUpdate: (projectId: string) => {
+        const queryClient = useQueryClient();
+        return useMutation({
+            mutationFn: (updatedFields: UpdateProjectDto) =>
+                updateProject(projectId, updatedFields),
+            onSuccess: () => {
+                return queryClient.invalidateQueries({
+                    queryKey: Project.queryKeys.all,
+                });
+            },
+        });
+    },
 };
+
+// TODO: add authorization tokens to config argument to these verbs wherever necessary
+// const config = {
+//     headers: {
+//         'Content-Type': 'application/json',
+//         Authorization: 'Bearer <token>',
+//     }
+// }
 
 const getProjects = async (): Promise<ProjectDocument[]> => {
     const response = await get('/api/projects');
@@ -30,5 +51,13 @@ const getProjects = async (): Promise<ProjectDocument[]> => {
 
 const getProjectById = async (projectId: string): Promise<ProjectDocument> => {
     const response = await get(`/api/projects/${projectId}`);
+    return response.data;
+};
+
+const updateProject = async (
+    projectId: string,
+    updatedFields: UpdateProjectDto,
+): Promise<ProjectDocument> => {
+    const response = await patch(`/api/projects/${projectId}`, updatedFields);
     return response.data;
 };
