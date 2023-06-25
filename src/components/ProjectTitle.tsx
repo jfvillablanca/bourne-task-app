d42d93c9b7a3ebf1ae6eb9d1c09a936adbbda006
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-06-25 10:36:33 +0800

    Feat: rework ProjectTitle to use PopOver

--- src/components/ProjectTitle.tsx
+++ src/components/ProjectTitle.tsx
@@ -1,7 +1,10 @@
+import { Check, Pencil } from 'lucide-react';
 import { HTMLAttributes, useEffect, useState } from 'react';
 
 import { Project } from '../api';
 import { cn } from '../lib/utils';
+
+import { Popover, PopoverClose, PopoverContent, PopoverTrigger } from './ui';
 
 interface ProjectTitleProps extends HTMLAttributes<HTMLDivElement> {
     projectId: string;
@@ -31,51 +34,65 @@ const ProjectTitleWrapped: React.FC<ProjectTitleProps> = ({
     className,
     projectId,
 }) => {
+    const [open, setOpen] = useState(false);
     const [title, setTitle] = useState('');
-    const [isEditMode, setIsEditMode] = useState(false);
+    const [editTitle, setEditTitle] = useState(title);
 
     const projectQuery = Project.useFindOne(projectId);
     const projectMutation = Project.useUpdate(projectId);
 
     const handleMutation = () => {
-        projectMutation.mutate({ title: title.trim() });
-        setIsEditMode(false);
+        setTitle(editTitle);
+        setOpen(false);
+        projectMutation.mutate({ title: editTitle.trim() });
     };
 
     useEffect(() => {
         if (projectQuery.isSuccess) {
             setTitle(() => projectQuery.data.title);
+            setEditTitle(() => projectQuery.data.title);
         }
     }, [projectQuery.isSuccess, projectQuery.data]);
 
     if (projectQuery.data) {
         return (
-            <div className={cn('', className)}>
-                {isEditMode ? (
-                    <input
-                        value={title}
-                        onChange={(e) => {
-                            e.preventDefault();
-                            setTitle(() => e.target.value);
-                        }}
-                        onBlur={handleMutation}
-                        onKeyDown={(e) => {
-                            if (e.key === 'Enter') {
-                                handleMutation();
-                            }
-                        }}
-                        autoFocus={true}
-                    />
-                ) : (
-                    <h2
-                        className="cursor-text"
-                        onClick={() => {
-                            setIsEditMode(true);
-                        }}
-                    >
-                        {title}
-                    </h2>
-                )}
+            <div className={cn('flex items-center gap-2 h-14', className)}>
+                <Popover open={open} onOpenChange={setOpen}>
+                    <PopoverTrigger asChild>
+                        <button
+                            className="btn btn-ghost flex gap-3 items-center data-[state=open]:bg-neutral"
+                            data-testid={`project-title-${projectQuery.data._id}`}
+                            onClick={() => setOpen((v) => !v)}
+                        >
+                            <h2 className="text-2xl font-medium tracking-tight normal-case">
+                                {title}
+                            </h2>
+                            <Pencil className="w-4" />
+                        </button>
+                    </PopoverTrigger>
+                    <PopoverContent className="flex gap-3 items-center rounded-lg p-2 bg-neutral shadow-md shadow-accent/20">
+                        <input
+                            className="input input-bordered input-accent w-full max-w-xs"
+                            value={editTitle}
+                            onChange={(e) => {
+                                setEditTitle(() => e.target.value);
+                            }}
+                            onKeyDown={(e) => {
+                                if (e.key === 'Enter') {
+                                    handleMutation();
+                                }
+                            }}
+                            data-testid={`project-input-edit-title-${projectQuery.data._id}`}
+                            autoFocus={true}
+                        />
+                        <PopoverClose
+                            className="btn btn-sm btn-circle btn-ghost hover:btn-accent"
+                            onClick={handleMutation}
+                        >
+                            <Check className="w-4" />
+                        </PopoverClose>
+                    </PopoverContent>
+                </Popover>
             </div>
         );
     }
