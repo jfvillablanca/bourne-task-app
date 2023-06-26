c30b60f854173b966baa0c4e495269663a1f22c4
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-06-26 18:42:21 +0800

    Feat: findOne task

--- src/api/Task.ts
+++ src/api/Task.ts
@@ -7,12 +7,20 @@ import { get } from '.';
 export const Task = {
     queryKeys: {
         all: (projectId: string) => ['tasks', projectId] as const,
+        byId: (projectId: string, taskId: string) =>
+            [...Task.queryKeys.all(projectId), taskId] as const,
     },
 
     useFindAll: (projectId: string) =>
         useQuery({
             queryKey: Task.queryKeys.all(projectId),
             queryFn: () => getTasks(projectId),
+        }),
+
+    useFindOne: (projectId: string, taskId: string) =>
+        useQuery({
+            queryKey: Task.queryKeys.byId(projectId, taskId),
+            queryFn: () => getTaskById(projectId, taskId),
         }),
 
     filterByTaskState: (allTasks: TaskDocument[], taskState: string) => {
@@ -22,5 +30,13 @@ export const Task = {
 
 const getTasks = async (projectId: string): Promise<TaskDocument[]> => {
     const response = await get(`/api/projects/${projectId}/tasks`);
+    return response.data;
+};
+
+const getTaskById = async (
+    projectId: string,
+    taskId: string,
+): Promise<TaskDocument> => {
+    const response = await get(`/api/projects/${projectId}/tasks/${taskId}`);
     return response.data;
 };
