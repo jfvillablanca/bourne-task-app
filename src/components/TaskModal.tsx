0e0c593cc5e37dfed8b023d7424f582490708e5d
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-06-26 20:53:12 +0800

    Feat: update Task details via useMutation

--- src/components/TaskModal.tsx
+++ src/components/TaskModal.tsx
@@ -1,7 +1,8 @@
-import { Pencil, X } from 'lucide-react';
-import { HTMLAttributes, useState } from 'react';
+import { Check, Pencil, X } from 'lucide-react';
+import { HTMLAttributes, useEffect, useState } from 'react';
 
-import { TaskDocument } from '../common';
+import { Task } from '../api';
+import { TaskDocument, UpdateTaskDto } from '../common';
 import { cn } from '../lib/utils';
 
 import {
@@ -26,6 +27,41 @@ const TaskModal: React.FC<TaskModalProps> = ({
     ...props
 }) => {
     const [open, setOpen] = useState(false);
+    const [taskForm, setTaskForm] = useState<UpdateTaskDto>({
+        title: '',
+        description: '',
+    });
+    const [editTaskForm, setEditTaskForm] = useState<UpdateTaskDto>(taskForm);
+
+    const taskQuery = Task.useFindOne(projectId, task._id);
+    const taskMutation = Task.useUpdate(projectId, task._id);
+
+    useEffect(() => {
+        if (taskQuery.isSuccess) {
+            setTaskForm(() => ({
+                title: taskQuery.data.title,
+                description: taskQuery.data.description,
+            }));
+            setEditTaskForm(() => ({
+                title: taskQuery.data.title,
+                description: taskQuery.data.description,
+            }));
+        }
+    }, [taskQuery.isSuccess, taskQuery.data]);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setEditTaskForm((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
+
+    const handleMutation = () => {
+        setTaskForm(() => editTaskForm);
+        setOpen(false);
+        taskMutation.mutate(editTaskForm);
+    };
 
     return (
         <Dialog open={open} onOpenChange={setOpen}>
@@ -46,23 +82,51 @@ const TaskModal: React.FC<TaskModalProps> = ({
                 data-testid={`task-modal-${task._id}`}
                 {...props}
             >
-                <div className="flex justify-between">
-                    <DialogHeading className="font-semibold text-lg">
-                        {task.title}
-                    </DialogHeading>
-                    <DialogClose className="btn btn-sm btn-circle btn-ghost">
-                        <X className="text-sm" />
-                    </DialogClose>
-                </div>
-                {task.description && <div className="divider mt-0 mb-2"></div>}
-                <DialogDescription className="">
-                    <p>{task.description}</p>
-                </DialogDescription>
-                <MemberAvatars
-                    className="w-8 h-8"
-                    projectId={projectId}
-                    taskMemberIds={task.assignedProjMemberId}
-                />
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        handleMutation();
+                    }}
+                >
+                    <div className="flex justify-between">
+                        <DialogHeading className="font-semibold text-lg">
+                            <label htmlFor="title">Title: </label>
+                            <input
+                                type="text"
+                                id="title"
+                                name="title"
+                                value={editTaskForm.title}
+                                onChange={handleChange}
+                                data-testid={`task-edit-title-${task._id}`}
+                            />
+                        </DialogHeading>
+                        <DialogClose className="btn btn-sm btn-circle btn-ghost">
+                            <X className="text-sm" />
+                        </DialogClose>
+                    </div>
+                    {task.description && (
+                        <div className="divider mt-0 mb-2"></div>
+                    )}
+                    <DialogDescription className="">
+                        <label htmlFor="Description">Description: </label>
+                        <input
+                            type="text"
+                            id="description"
+                            name="description"
+                            value={editTaskForm.description}
+                            onChange={handleChange}
+                            data-testid={`task-edit-description-${task._id}`}
+                        />
+                    </DialogDescription>
+                    <MemberAvatars
+                        className="w-8 h-8"
+                        projectId={projectId}
+                        taskMemberIds={task.assignedProjMemberId}
+                    />
+                    <button type="submit">
+                        <Check />
+                    </button>
+                </form>
             </DialogContent>
         </Dialog>
     );
