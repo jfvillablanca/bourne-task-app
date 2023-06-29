1feb6370f755520621862baff4cdc9687fdde864
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-06-29 10:01:06 +0800

    Feat: render Skeleton in Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,6 +5,7 @@ import { UseQueryResult } from '@tanstack/react-query';
 import { ProjectDocument } from '../common';
 import { cn } from '../lib/utils';
 
+import { Skeleton } from './ui';
 import { ProjectAdd } from '.';
 
 interface SidebarProps extends HTMLAttributes<HTMLDivElement> {
@@ -18,38 +19,36 @@ const Sidebar: React.FC<SidebarProps> = ({
     handleClickedProject,
     ...props
 }) => {
-    if (projectQuery && projectQuery.data) {
-        return (
-            <div className={cn('flex flex-col px-3', className)} {...props}>
-                <h2 className="font-bold text-2xl self-center tracking-wider cursor-default">
-                    My Projects
-                </h2>
-                <div className="divider my-1"></div>
-                <ul className="menu gap-2">
-                    {projectQuery.data.map((project) => (
-                        <li
-                            className="btn btn-ghost text-lg font-semibold capitalize h-min"
-                            key={project._id}
-                            onClick={() => {
-                                handleClickedProject(project._id);
-                            }}
-                        >
-                            {project.title}
-                        </li>
-                    ))}
-                </ul>
-                <ProjectAdd className="mx-2" />
-            </div>
-        );
-    }
-
     return (
         <div className={cn('flex flex-col px-3', className)} {...props}>
+            <h2 className="font-bold text-2xl self-center tracking-wider cursor-default">
+                My Projects
+            </h2>
+            <div className="divider my-1"></div>
             <ul className="menu gap-2">
-                {Array.from({ length: 3 }).map((_, i) => {
-                    return <li key={i}>Loading...</li>;
-                })}
+                {projectQuery && projectQuery.data
+                    ? projectQuery.data.map((project) => (
+                          <li
+                              className="btn btn-ghost text-lg font-semibold capitalize h-min"
+                              key={project._id}
+                              onClick={() => {
+                                  handleClickedProject(project._id);
+                              }}
+                          >
+                              {project.title}
+                          </li>
+                      ))
+                    : Array.from({ length: 3 }).map((_, i) => {
+                          return (
+                              <li key={i}>
+                                  <Skeleton className="h-8 mb-1" />
+                              </li>
+                          );
+                      })}
             </ul>
+            {projectQuery && projectQuery.data && (
+                <ProjectAdd className="mx-2" />
+            )}
         </div>
     );
 };
