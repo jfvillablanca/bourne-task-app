9cd8785f9f1b0e53822563ebd70899a54b00a50e
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-07-02 09:12:57 +0800

    Feat: render FormTaskMembers as a Dialog

--- src/components/FormTaskMembers.tsx
+++ src/components/FormTaskMembers.tsx
@@ -1,8 +1,10 @@
 import { clsx } from 'clsx';
 import { PlusCircle } from 'lucide-react';
+import { useState } from 'react';
 import Select, { ActionMeta, MultiValue } from 'react-select';
 
 import { FormChangeType, ProjectMember } from '../common';
+import { Dialog, DialogClose, DialogContent, DialogTrigger } from './ui';
 
 type OptionType = { label: string; value: string; _id: string };
 
@@ -17,6 +19,7 @@ const FormTaskMembers = ({
     value,
     handleChange,
 }: FormTaskMembersProps) => {
+    const [open, setOpen] = useState(false);
     const allProjectMembers: OptionType[] = projectMembers.map((member) => {
         return { label: member.email, value: member.email, _id: member._id };
     });
@@ -47,33 +50,38 @@ const FormTaskMembers = ({
     };
 
     return (
-        <div className="dropdown">
-            <button
-                className="btn btn-ghost rounded-full text-neutral-content hover:text-accent transition-colors"
-                data-testid={'open-select-update-assigned'}
-                onClick={(e) => e.preventDefault()}
-            >
-                <PlusCircle className="" />
-            </button>
-
-            <Select
-                className="select h-20 min-w-[10rem] dropdown-content z-[1] p-2"
-                data-testid={'select-update-assigned'}
-                classNames={{
-                    control: ({ isFocused }) =>
-                        clsx(
-                            'border w-max rounded-lg',
-                            isFocused ? 'border-accent' : 'border-base-content',
-                        ),
-                }}
-                defaultValue={selectedTaskMembers}
-                options={allProjectMembers}
-                isMulti
-                // unstyled
-                onChange={handleMenuChange}
-                aria-label="select to assign project members to this task"
-            />
-        </div>
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogTrigger asChild>
+                <button
+                    className="btn btn-ghost rounded-full text-neutral-content hover:text-accent transition-colors"
+                    data-testid={'open-select-update-assigned'}
+                    onClick={(e) => {
+                        e.preventDefault();
+                        setOpen((v) => !v);
+                    }}
+                >
+                    <PlusCircle />
+                </button>
+            </DialogTrigger>
+            <DialogContent className="relative z-10 bg-base-100 h-max transition">
+                <Select
+                    className="max-w-[40rem] z-10 min-w-[30rem] min-h-[2rem]"
+                    data-testid={'select-update-assigned'}
+                    autoFocus
+                    defaultValue={selectedTaskMembers}
+                    placeholder={'Select members to assign'}
+                    options={allProjectMembers}
+                    isMulti
+                    unstyled
+                    onChange={handleMenuChange}
+                    closeMenuOnSelect={false}
+                    aria-label="select to assign project members to this task"
+                />
+                <DialogClose className="absolute -bottom-7 right-0 pt-2 z-0 rounded-b-lg border border-neutral-content flex w-20 min-w-[4rem] justify-center text-neutral-content hover:bg-success hover:text-success-content">
+                    <Check className="mr-1" />
+                </DialogClose>
+            </DialogContent>
+        </Dialog>
     );
 };
 
