8e15e4e3ed12b4802f8155ef47d42e94ea5f66e9
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-07-03 05:39:43 +0800

    Refactor: inline unnecessary type declaration

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,11 @@ import { HTMLAttributes } from 'react';
 
 import { cn } from '../lib/utils';
 
-type HeaderProps = HTMLAttributes<HTMLDivElement>;
+const Header: React.FC<HTMLAttributes<HTMLDivElement>> = ({
+    className,
+    ...props
+}) => {
 
-const Header: React.FC<HeaderProps> = ({ className, ...props }) => {
     return (
         <div className={cn('flex p-4 items-center', className)} {...props}>
             <GanttChart className="h-12 w-max mr-2" />
