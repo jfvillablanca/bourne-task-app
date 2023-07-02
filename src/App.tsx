0cfdcb232ebe8dab507d56b4f91173397049b5bf
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-07-02 18:35:55 +0800

    Refactor: flatten the grid children

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,14 @@ import { useState } from 'react';
 import { useEffectOnce } from 'usehooks-ts';
 
 import { Project } from './api';
-import { CardView, ProjectTitle, Sidebar, ToastContainer } from './components';
+import {
+    CardView,
+    Header,
+    ProjectTitle,
+    Sidebar,
+    ToastContainer,
+} from './components';
+
 function App() {
     const [selectedProject, setSelectedProject] = useState<string | null>(null);
 
@@ -23,24 +30,23 @@ function App() {
     return (
         <>
             <ToastContainer />
-            <div className="grid grid-cols-5 py-5 h-screen text-lg">
+            <div className="grid auto-rows-min h-screen text-lg">
+                <Header className="row-start-1 col-span-2 border-b border-base-300 -mb-[1px]" />
                 <Sidebar
-                    className="col-span-1"
+                    className="row-start-3 row-span-2"
                     projectQuery={projectsQuery}
                     handleClickedProject={handleClickedProject}
                 />
-                <div className="col-span-4">
-                    <ProjectTitle
-                        className="mb-4"
+                <ProjectTitle
+                    className="row-start-2 row-span-1 col-start-2 mb-4"
+                    projectId={selectedProject}
+                />
+                {selectedProject && (
+                    <CardView
+                        className="row-start-3 row-span-1 h-full"
                         projectId={selectedProject}
                     />
-                    {selectedProject && (
-                        <CardView
-                            className="h-full"
-                            projectId={selectedProject}
-                        />
-                    )}
-                </div>
+                )}
             </div>
         </>
     );
