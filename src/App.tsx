995c74d70e0405e59619f7eaefafd43feee6c4cb
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-07-04 08:11:09 +0800

    Style: switch to flex; grid fails to fill screen

--- src/App.tsx
+++ src/App.tsx
@@ -30,18 +30,26 @@ function App() {
     return (
         <>
             <ToastContainer />
-            <div className="grid auto-rows-min h-screen text-lg bg-base-200">
-                <Header className="row-start-1 col-span-2 border-b border-base-300" />
-                <Sidebar
-                    className="row-start-2 row-span-1 w-max px-2 py-4 border-r border-base-300"
-                    projectQuery={projectsQuery}
-                    handleClickedProject={handleClickedProject}
-                />
-                <div className="row-start-2 col-start-2 bg-base-100 overflow-x-auto">
-                    <ProjectTitle className="m-4" projectId={selectedProject} />
-                    {selectedProject && (
-                        <CardView className="p-4" projectId={selectedProject} />
-                    )}
+            <div className="flex flex-col h-screen text-lg bg-base-200">
+                <Header className="border-b border-base-300" />
+                <div className="flex-1 flex">
+                    <Sidebar
+                        className="w-max px-2 py-4 border-r border-base-300"
+                        projectQuery={projectsQuery}
+                        handleClickedProject={handleClickedProject}
+                    />
+                    <div className="flex-1 px-4 bg-base-100 overflow-x-auto">
+                        <ProjectTitle
+                            className="m-4"
+                            projectId={selectedProject}
+                        />
+                        {selectedProject && (
+                            <CardView
+                                className="p-4"
+                                projectId={selectedProject}
+                            />
+                        )}
+                    </div>
                 </div>
             </div>
         </>
