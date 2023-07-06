7d68a8687296b5f193a363c5fc4a7ed2cfedff50
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-07-06 16:01:36 +0800

    Feat: add Login tab in AuthModal

--- src/components/AuthenticationModal.tsx
+++ src/components/AuthenticationModal.tsx
@@ -21,16 +21,8 @@ const AuthenticationModal: React.FC<HTMLAttributes<HTMLDivElement>> = ({
     className,
     ...props
 }) => {
-
-    const {
-        errors,
-        getValues,
-        handleSubmit,
-        onSubmit,
-        register,
-        registerMutation,
-    } = useRegister({ setOpen });
     const [open, toggleOpen, setOpen] = useToggle();
+    const [isLogin /* toggleIsLogin */, , setIsLogin] = useToggle();
 
     return (
         <Dialog open={open} onOpenChange={setOpen}>
@@ -48,115 +40,47 @@ const AuthenticationModal: React.FC<HTMLAttributes<HTMLDivElement>> = ({
             </DialogTrigger>
             <DialogContent
                 className={cn(
-                    'justify-center items-center h-[50vh] border p-10 backdrop-filter backdrop-blur-xl',
+                    'flex flex-col justify-center items-center h-[50vh] border p-10 backdrop-filter backdrop-blur-xl',
                     className,
                 )}
                 overlayClassName="bg-base-100/80"
                 {...props}
             >
-                <form
-                    className="form-control justify-center gap-2 h-full"
-                    onSubmit={handleSubmit(onSubmit)}
-                >
-                    <div className="flex flex-col space-y-2 mb-3 text-center">
-                        <h1 className="text-2xl font-semibold tracking-tight">
-                            Create an account
-                        </h1>
-                        <p className="text-sm text-base-content/60">
-                            Enter your email below to create your account
-                        </p>
-                    </div>
-                    <div className="grid gap-3">
-                        <div className="grid gap-1">
-                            <label className="sr-only" htmlFor="email">
-                                Email
-                            </label>
-                            <input
-                                className={`input input-primary focus:input-accent placeholder:text-sm ${
-                                    registerMutation.error ? 'input-error' : ''
-                                }`}
-                                id="email"
-                                type="email"
-                                placeholder="name@example.com"
-                                {...register('email', {
-                                    required: 'Email is required',
-                                    pattern: {
-                                        value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                                        message: 'Invalid email address',
-                                    },
-                                    onChange: () => registerMutation.reset(),
-                                })}
-                            />
-                            {errors.email && (
-                                <span className="text-error">
-                                    {errors.email.message}
-                                </span>
-                            )}
-                            {registerMutation.error && (
-                                <span className="text-error">
-                                    {registerMutation.error.statusText}
-                                </span>
-                            )}
-                        </div>
-                        <div className="grid gap-1">
-                            <input
-                                className="input input-primary focus:input-accent placeholder:text-sm"
-                                type="password"
-                                placeholder="Password"
-                                {...register('password', {
-                                    required: 'Password is required',
-                                })}
-                            />
-                            {errors.password && (
-                                <span className="text-error">
-                                    {errors.password.message}
-                                </span>
-                            )}
-                        </div>
-                        <div className="grid gap-1">
-                            <input
-                                className="input input-primary focus:input-accent placeholder:text-sm"
-                                type="password"
-                                placeholder="Confirm password"
-                                {...register('confirmPassword', {
-                                    required: 'Confirm password is required',
-                                    validate: (value) => {
-                                        return (
-                                            value === getValues('password') ||
-                                            'Passwords do not match'
-                                        );
-                                    },
-                                })}
-                            />
-                            {errors.confirmPassword && (
-                                <span className="text-error">
-                                    {errors.confirmPassword.message}
-                                </span>
-                            )}
-                        </div>
-                        <button
-                            className="btn btn-outline text-lg normal-case"
-                            type="submit"
-                            disabled={registerMutation.isLoading}
-                        >
-                            {registerMutation.isLoading ? (
-                                <div className="loading loading-infinity loading-lg"></div>
-                            ) : (
-                                'Sign up with email'
-                            )}
-                        </button>
-                    </div>
-                </form>
+                <div className="tabs">
+                    <a
+                        className={`tab tab-bordered ${
+                            isLogin ? '' : 'tab-active'
+                        }`}
+                        onClick={() => setIsLogin(false)}
+                        aria-label="register tab"
+                    >
+                        Register
+                    </a>
+                    <a
+                        className={`tab tab-bordered ${
+                            isLogin ? 'tab-active' : ''
+                        }`}
+                        onClick={() => setIsLogin(true)}
+                        aria-label="login tab"
+                    >
+                        Login
+                    </a>
+                </div>
+                {isLogin ? (
+                    <LoginForm setOpen={setOpen} />
+                ) : (
+                    <RegisterForm setOpen={setOpen} />
+                )}
             </DialogContent>
         </Dialog>
     );
 };
 
-const useRegister = ({
-    setOpen,
-}: {
+interface AuthFormProps extends HTMLAttributes<HTMLDivElement> {
     setOpen: React.Dispatch<React.SetStateAction<boolean>>;
-}) => {
+}
+
+const RegisterForm = ({ className, setOpen }: AuthFormProps) => {
     const {
         register,
         handleSubmit,
@@ -189,14 +113,225 @@ const useRegister = ({
         }
     }, [reset, registerMutation.isSuccess, setOpen]);
 
-    return {
-        errors,
-        getValues,
-        handleSubmit,
-        onSubmit,
+    return (
+        <form
+            className={cn(
+                'form-control justify-center gap-2 h-full',
+                className,
+            )}
+            onSubmit={handleSubmit(onSubmit)}
+        >
+            <div className="flex flex-col space-y-2 mb-3 text-center">
+                <h1 className="text-2xl font-semibold tracking-tight">
+                    Create an account
+                </h1>
+                <p className="text-sm text-base-content/60">
+                    Enter your email below to create your account
+                </p>
+            </div>
+            <div className="grid gap-3">
+                <div className="grid gap-1">
+                    <label className="sr-only" htmlFor="email">
+                        Email
+                    </label>
+                    <input
+                        className={`input input-primary focus:input-accent placeholder:text-sm ${
+                            registerMutation.error ? 'input-error' : ''
+                        }`}
+                        id="email"
+                        type="email"
+                        placeholder="name@example.com"
+                        {...register('email', {
+                            required: 'Email is required',
+                            pattern: {
+                                value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                                message: 'Invalid email address',
+                            },
+                            onChange: () => registerMutation.reset(),
+                        })}
+                    />
+                    {errors.email && (
+                        <span className="text-error">
+                            {errors.email.message}
+                        </span>
+                    )}
+                    {registerMutation.error && (
+                        <span className="text-error">
+                            {registerMutation.error.statusText}
+                        </span>
+                    )}
+                </div>
+                <div className="grid gap-1">
+                    <input
+                        className="input input-primary focus:input-accent placeholder:text-sm"
+                        type="password"
+                        placeholder="Password"
+                        {...register('password', {
+                            required: 'Password is required',
+                        })}
+                    />
+                    {errors.password && (
+                        <span className="text-error">
+                            {errors.password.message}
+                        </span>
+                    )}
+                </div>
+                <div className="grid gap-1">
+                    <input
+                        className="input input-primary focus:input-accent placeholder:text-sm"
+                        type="password"
+                        placeholder="Confirm password"
+                        {...register('confirmPassword', {
+                            required: 'Confirm password is required',
+                            validate: (value) => {
+                                return (
+                                    value === getValues('password') ||
+                                    'Passwords do not match'
+                                );
+                            },
+                        })}
+                    />
+                    {errors.confirmPassword && (
+                        <span className="text-error">
+                            {errors.confirmPassword.message}
+                        </span>
+                    )}
+                </div>
+                <button
+                    className="btn btn-outline text-lg normal-case"
+                    type="submit"
+                    disabled={registerMutation.isLoading}
+                >
+                    {registerMutation.isLoading ? (
+                        <div className="loading loading-infinity loading-lg"></div>
+                    ) : (
+                        'Sign up with email'
+                    )}
+                </button>
+            </div>
+        </form>
+    );
+};
+
+const LoginForm = ({ className, setOpen }: AuthFormProps) => {
+    const {
         register,
-        registerMutation,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm<AuthDto>({
+        mode: 'onChange',
+        defaultValues: {
+            email: '',
+            password: '',
+        },
+    });
+
+    const loginMutation = Auth.useLoginLocal();
+
+    const onSubmit: SubmitHandler<AuthDto> = async (formData) => {
+        const authDto: AuthDto = {
+            email: formData.email,
+            password: formData.password,
+        };
+        loginMutation.mutate(authDto);
     };
+
+    useEffect(() => {
+        if (loginMutation.isSuccess) {
+            reset();
+            setOpen(false);
+        }
+    }, [reset, loginMutation.isSuccess, setOpen]);
+
+    return (
+        <form
+            className={cn(
+                'form-control justify-center gap-2 h-full',
+                className,
+            )}
+            onSubmit={handleSubmit(onSubmit)}
+        >
+            <div className="flex flex-col space-y-2 mb-3 text-center">
+                <h1 className="text-2xl font-semibold tracking-tight">
+                    Log in
+                </h1>
+                <p className="text-sm text-base-content/60">
+                    Welcome back! Enter your credentials to login
+                </p>
+            </div>
+            <div className="grid gap-3">
+                <div className="grid gap-1">
+                    <label className="sr-only" htmlFor="email">
+                        Email
+                    </label>
+                    <input
+                        className={`input input-primary focus:input-accent placeholder:text-sm ${
+                            loginMutation.error?.type === 'user'
+                                ? 'input-error'
+                                : ''
+                        }`}
+                        id="email"
+                        type="email"
+                        placeholder="name@example.com"
+                        {...register('email', {
+                            required: 'Email is required',
+                            pattern: {
+                                value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                                message: 'Invalid email address',
+                            },
+                            onChange: () => loginMutation.reset(),
+                        })}
+                    />
+                    {errors.email && (
+                        <span className="text-error">
+                            {errors.email.message}
+                        </span>
+                    )}
+                    {loginMutation.error?.type === 'user' && (
+                        <span className="text-error">
+                            {loginMutation.error.statusText}
+                        </span>
+                    )}
+                </div>
+                <div className="grid gap-1">
+                    <input
+                        className={`input input-primary focus:input-accent placeholder:text-sm ${
+                            loginMutation.error?.type === 'password'
+                                ? 'input-error'
+                                : ''
+                        }`}
+                        type="password"
+                        placeholder="Password"
+                        {...register('password', {
+                            required: 'Password is required',
+                        })}
+                    />
+                    {errors.password && (
+                        <span className="text-error">
+                            {errors.password.message}
+                        </span>
+                    )}
+                    {loginMutation.error?.type === 'password' && (
+                        <span className="text-error">
+                            {loginMutation.error.statusText}
+                        </span>
+                    )}
+                </div>
+                <button
+                    className="btn btn-outline text-lg normal-case"
+                    type="submit"
+                    disabled={loginMutation.isLoading}
+                >
+                    {loginMutation.isLoading ? (
+                        <div className="loading loading-infinity loading-lg"></div>
+                    ) : (
+                        'Login'
+                    )}
+                </button>
+            </div>
+        </form>
+    );
 };
 
 export default AuthenticationModal;
