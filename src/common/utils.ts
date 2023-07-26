b842e0d1df510c421a9c0402a16c0bdb4624e1fe
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-07-26 12:56:21 +0800

    Chore: add expiration-related utils function

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,2 +1,14 @@
+import { millisecondsToSeconds, secondsToMilliseconds } from 'date-fns';
+
+import { decodeToken } from '../lib/utils';
+
 export const delay = (ms: number) =>
     new Promise((resolve) => setTimeout(resolve, ms));
+
+export function expirationDate(access_token: string) {
+    return secondsToMilliseconds(decodeToken(access_token).exp);
+}
+
+export function secondsBeforeExpiration(access_token: string) {
+    return millisecondsToSeconds(expirationDate(access_token) - Date.now());
+}
