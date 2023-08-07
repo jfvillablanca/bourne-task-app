70a7c5d5aa9766f5ee5eca50a8568fb0ea059b36
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-08-07 11:44:58 +0800

    Fix: use named import instead of default import

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
 
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
