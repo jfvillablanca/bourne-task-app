551c443cd15e9deb7bb0ac72162a3c3066e470f5
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-09-02 09:18:03 +0800

    Feat: enable onHover 'email' tooltip for Avatar

--- src/components/ui/Avatar.tsx
+++ src/components/ui/Avatar.tsx
@@ -3,17 +3,31 @@ import { HTMLAttributes } from 'react';
 import { User } from '../../common';
 import { cn, generateAvatarURL } from '../../lib/utils';
 
-export const Avatar: React.FC<
-    HTMLAttributes<HTMLDivElement> & { email: User['email'] }
-> = ({ email, className }) => {
+import { Tooltip, TooltipContent, TooltipTrigger } from '.';
+
+interface AvatarProps extends HTMLAttributes<HTMLDivElement> {
+    email: User['email'];
+    showTooltipOnHover?: boolean;
+}
+
+export const Avatar: React.FC<AvatarProps> = ({
+    className,
+    email,
+    showTooltipOnHover = true,
+}) => {
     return (
-        <div
-            className={cn(
-                'avatar h-11 bg-base-300 rounded-full border-2 border-accent-content',
-                className,
-            )}
-        >
-            <img src={generateAvatarURL(email)} alt={email} />
-        </div>
+        <Tooltip>
+            <TooltipTrigger>
+                <div
+                    className={cn(
+                        'avatar cursor-auto h-11 bg-base-300 rounded-full border-2 border-accent-content',
+                        className,
+                    )}
+                >
+                    <img src={generateAvatarURL(email)} alt={email} />
+                </div>
+            </TooltipTrigger>
+            {showTooltipOnHover && <TooltipContent>{email}</TooltipContent>}
+        </Tooltip>
     );
 };
